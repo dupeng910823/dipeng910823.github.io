3e753f909deab3033fae34412b6e9ae4d794011c
dupeng <1961455515@qq.com>
2019-08-15 19:13:45 +0800

    no message

--- assets/js/loop.mini.app.js
+++ assets/js/loop.mini.app.js
@@ -24,16 +24,17 @@
     }  
 
     function setupWebViewJavascriptBridge(callback) {
-        if (this.window.WebViewJavascriptBridge) { return callback(this.window.WebViewJavascriptBridge); }else {
-            setTimeout(function () { setupWebViewJavascriptBridge(callback) }, 100)
-        }
-        // if (this.window.WVJBCallbacks) { return this.window.WVJBCallbacks.push(callback); }
-        // this.window.WVJBCallbacks = [callback];
-        // var WVJBIframe = document.createElement('iframe');
-        // WVJBIframe.style.display = 'none';
-        // WVJBIframe.src = 'https://__bridge_loaded__';
-        // document.documentElement.appendChild(WVJBIframe);
-        // setTimeout(function () { document.documentElement.removeChild(WVJBIframe) }, 0)
+        if (this.window.WebViewJavascriptBridge) { return callback(this.window.WebViewJavascriptBridge); }
+        // else {
+        //     setTimeout(function () { setupWebViewJavascriptBridge(callback) }, 100)
+        // }
+        if (this.window.WVJBCallbacks) { return this.window.WVJBCallbacks.push(callback); }
+        this.window.WVJBCallbacks = [callback];
+        var WVJBIframe = document.createElement('iframe');
+        WVJBIframe.style.display = 'none';
+        WVJBIframe.src = 'https://__bridge_loaded__';
+        document.documentElement.appendChild(WVJBIframe);
+        setTimeout(function () { document.documentElement.removeChild(WVJBIframe) }, 0)
     }
     win.loop = new LOOP();
 
